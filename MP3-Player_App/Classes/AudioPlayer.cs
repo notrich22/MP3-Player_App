2b5ab4944ef966ad8715619e21bf7dff93f1e914
notrich22 <notrich22@mail.ru>
2023-10-07 17:37:42 +0200

    Logic Almost Done

---
+++ MP3-Player_App/Classes/AudioPlayer.cs
@@ -0,0 +1,198 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace MP3_Player_App
+{
+    public class AudioPlayer
+    {
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFileReader;
+        string[] audioFileExtensions = { ".mp3", ".wav", ".flac" };
+        private string songTitle;
+        private string artist;
+
+        private bool isPlaying = false;
+        private float volume = 0.75f;
+
+        private List<string> filePaths;
+
+        public event EventHandler<bool> PlaybackStateChanged;
+        public List<string> Playlist { 
+            get { return filePaths; }
+            private set { filePaths = value; }
+        }
+        public TimeSpan CurrentTime { 
+            get
+            {
+                if (audioFileReader != null && outputDevice != null && outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    return audioFileReader.CurrentTime;
+                }
+                else { return TimeSpan.Zero; }
+            }
+            set
+            {
+                if (audioFileReader != null)
+                {
+                    audioFileReader.CurrentTime = value;
+                }
+            }
+        }
+        public bool IsPlaying { 
+            get {
+                return isPlaying;
+            }
+            set
+            {
+                isPlaying = value;
+            }
+        }
+        public bool IsSelected
+        {
+            get
+            {
+                return !String.IsNullOrEmpty(selectedFilePath);
+            }
+        }
+        public float Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (volume != value)
+                {
+                    volume = value;
+                    if (audioFileReader != null)
+                    {
+                        audioFileReader.Volume = volume;
+                    }
+                }
+            }
+        }
+        public string selectedFilePath { get; set; }
+        public string SongTitle
+        {
+            get {
+                if (IsSelected)
+                {
+                    using (var mp3File = new Mp3FileReader(selectedFilePath))
+                    {
+                        if (mp3File != null && mp3File.Id3v2Tag != null)
+                        {
+                            // Пытаемся получить название песни из метаданных
+                            if (!string.IsNullOrEmpty(mp3File.Id3v2Tag.ToString()))
+                            {
+                                return mp3File.Id3v2Tag.ToString();
+                            }
+                        }
+                        return Path.GetFileNameWithoutExtension(selectedFilePath);
+                    }
+                }
+                return "";
+            }
+            
+        }
+        public string Artist
+        {
+            get { return artist; }
+            
+        }
+        public AudioPlayer()
+        {
+            outputDevice = new WaveOutEvent();
+            Playlist = new List<string>();
+            AddFolderToPlaylist(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Music"));
+            if(Playlist.Count > 0) {
+                //selectedFilePath = Playlist[0];
+            }
+            OpenSelectedFile();
+        }
+
+        public double getTrackTotalTime()
+        {
+            if (IsSelected) { 
+                return audioFileReader.TotalTime.TotalSeconds;
+            }
+            return TimeSpan.Zero.TotalSeconds;
+        }
+        public int? getTrackBitrate()
+        {
+            using (var reader = new Mp3FileReader(selectedFilePath))
+            {
+                if (reader != null)
+                {
+                    return reader.Mp3WaveFormat.AverageBytesPerSecond * 8 / 1000 + 1;
+                }
+            }
+            return null;
+        }
+        public void OpenSelectedFile()
+        {
+            if(selectedFilePath != null) { 
+                audioFileReader = new AudioFileReader(selectedFilePath);
+                audioFileReader.Volume = Volume;
+                outputDevice.Init(audioFileReader);
+            }
+        }
+        public void AddFolderToPlaylist(string folderPath)
+        {
+            foreach (string filePath in Directory.GetFiles(folderPath)) {
+                if (audioFileExtensions.Contains(Path.GetExtension(filePath), StringComparer.OrdinalIgnoreCase))
+                {
+                    Playlist.Add(filePath);
+                }
+            }
+        }
+
+        public void Play()
+        {
+            if (outputDevice != null && audioFileReader != null)
+            {
+                outputDevice.Play();
+                isPlaying = true;
+                PlaybackStateChanged?.Invoke(this, true);
+            }
+        }
+
+        public void Pause()
+        {
+            if (outputDevice != null)
+            {
+                outputDevice.Pause();
+                isPlaying = false;
+                PlaybackStateChanged?.Invoke(this, false);
+            }
+        }
+
+        public void Stop()
+        {
+            if (outputDevice != null && isPlaying)
+            {
+                outputDevice.Stop();
+                isPlaying = false;
+                PlaybackStateChanged?.Invoke(this, false);
+            }
+        }
+
+
+        public async Task PlayAsync()
+        {
+            await Task.Run(() => Play());
+        }
+
+        public async Task PauseAsync()
+        {
+            await Task.Run(() => Pause());
+        }
+
+        public async Task StopAsync()
+        {
+            await Task.Run(() => Stop());
+        }
+    }
+}
--- MP3-Player_App/MainWindow.xaml.cs
+++ MP3-Player_App/MainWindow.xaml.cs
@@ -1,119 +1,197 @@
 ﻿using System;
 using System.Windows;
 using NAudio.Wave;
-using NAudio.Lame;
 using System.Windows.Threading;
+using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel;
 using System.Windows.Controls;
-using NAudio.WaveFormRenderer;
-using System.Media;
+using System.Drawing;
+using NAudio.Wave;
+using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using NAudio.Dsp;
+using System.Windows.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Timers;
+using System.Diagnostics;
 
 namespace MP3_Player_App
 {
     public partial class MainWindow : Window
     {
-        private WaveOutEvent outputDevice;
-        private AudioFileReader audioFileReader;
-        private bool isPlaying = false;
-        private string filePath = "D:\\Downloads\\Danzig - Mother .mp3";
-        private float volume = 1.0f;
-        private DispatcherTimer timer;
+        AudioPlayer audioPlayer;
         public float Volume
         {
-            get { return volume; }
-            set
+            get { return audioPlayer.Volume; }
+            set { audioPlayer.Volume = value; }
+        }
+        public string SongTitle
+        {
+            get { return audioPlayer.SongTitle; }
+        }
+        public string Artist
+        {
+            get { return audioPlayer.Artist; }
+        }
+        public bool IsSelected
+        {
+            get
             {
-                if (value < 0.0f)
-                    volume = 0.0f;
-                else if (value > 1.0f)
-                    volume = 1.0f;
-                else
-                    volume = value;
-
-                if (audioFileReader != null)
-                {
-                    audioFileReader.Volume = volume;
-                }
+                return audioPlayer.IsSelected;
             }
         }
+        private DispatcherTimer timer;
 
         public MainWindow()
         {
+            
+            audioPlayer = new AudioPlayer();
             InitializeComponent();
             DataContext = this;
-            Volume = 0.75f;
-            TrackPositionSlider.Minimum = 0;
-            TrackPositionSlider.Value = 0; // Устанавливаем начальное значение в начало трека.
+            UpdateListBox();
+            TrackSlider.Minimum = 0;
+            InitializeTimer();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.01);
+            timer.Tick += async (sender, e) =>
+            {
+                await UpdateGUIAsync(sender, e);
+            };
 
+            timer.Start();
+        }
+        private async Task UpdateGUIAsync(object sender, EventArgs e)
+        {
+            if (IsSelected)
+            {
+                ControlPanel.IsEnabled = true;
+            }
+            else
+            {
+                ControlPanel.IsEnabled = false;
+            }
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            TrackPositionSlider.Minimum = 0;
-            TrackPositionSlider.Maximum = audioFileReader.TotalTime.TotalSeconds;
-
-            InitializeTimer();
+            TrackSlider.Minimum = 0;
         }
-
-
-        private void BitrateComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void UpdateListBox()
         {
-
-        }
-        private void InitializeTimer()
-        {
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += Timer_Tick;
-            timer.Start();
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            if (audioFileReader != null)
+            TrackListBox.Items.Clear();
+            foreach (var item in audioPlayer.Playlist)
             {
-                TrackPositionSlider.Value = audioFileReader.CurrentTime.TotalSeconds;
+                TrackListBox.Items.Add(Path.GetFileName(item));
             }
+
         }
 
-        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        private string UpdateTrackBitrate(string filePath)
         {
-            if (isPlaying)
+            if (audioPlayer.getTrackBitrate() != null)
             {
-                if (outputDevice != null)
+                return "Битрейт трека: " + audioPlayer.getTrackBitrate() + " kbps";
+            }
+            return "Не удалось получить битрейт";
+        }
+
+        private async void InitializeTimer()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
                 {
-                    outputDevice.Pause();
-                    isPlaying = false;
+                    Dispatcher.Invoke(() =>
+                    {
+                        if (IsSelected)
+                        {
+                            TrackSlider.Value = audioPlayer.CurrentTime.TotalSeconds;
+                            TrackSlider.Maximum = audioPlayer.getTrackTotalTime();
+                        }
+                    });
                 }
+            });
+        }
+
+        private async void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (audioPlayer.IsPlaying)
+            {
+                await audioPlayer.PauseAsync();
+                audioPlayer.IsPlaying = false;
             }
             else
             {
-                if (outputDevice == null)
-                {
-                    outputDevice = new WaveOutEvent();
-                    audioFileReader = new AudioFileReader(filePath);
-                    outputDevice.Init(audioFileReader);
-                    TrackPositionSlider.Maximum = audioFileReader.TotalTime.TotalSeconds; // Обновляем максимальное значение ползунка при загрузке трека.
-                }
-                outputDevice.Play();
-                isPlaying = true;
+                
+                BitrateTextBlock.Visibility = Visibility.Visible;
+                BitrateTextBlock.Text = UpdateTrackBitrate(audioPlayer.selectedFilePath);
+                await audioPlayer.PlayAsync();
+                audioPlayer.IsPlaying = true;
             }
         }
 
+
+        private void TrackSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double newPosition = TrackSlider.Value;
+            audioPlayer.CurrentTime = TimeSpan.FromSeconds(newPosition);
+        }
+
+        private void SelectDirectoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Выберите папку с аудиофайлами";
+                folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    string selectedFolder = folderBrowserDialog.SelectedPath;
+                    audioPlayer.AddFolderToPlaylist(selectedFolder);
+                    UpdateListBox();
+                }
+            }
+        }
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
-            if (outputDevice != null)
-            {
-                outputDevice.Stop();
-                outputDevice.Dispose();
-                outputDevice = null;
-                isPlaying = false;
-            }
+            audioPlayer.Stop();
+        }
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
 
-        private void TrackPositionSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            double newPosition = TrackPositionSlider.Value;
-            audioFileReader.CurrentTime = TimeSpan.FromSeconds(newPosition);
+            audioPlayer.Volume = (float)VolumeSlider.Value;
+        }
+        private async void TrackListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TrackSlider.IsEnabled = true;
+            await audioPlayer.StopAsync();
+            if (TrackListBox.SelectedItem != null)
+            {
+                foreach (var filePath in audioPlayer.Playlist)
+                {
+                    if (Path.GetFileName(filePath) == TrackListBox.SelectedItem.ToString()) {
+                        audioPlayer.selectedFilePath = filePath;
+                        audioPlayer.OpenSelectedFile();
+                    }
+                }
+            }
         }
 
     }
